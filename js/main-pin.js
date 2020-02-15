27775377fd993512e847516e9428a47cc7c82ed2
Ratibor <klov29@mail.ru>
2020-02-15 19:15:05 +0700

    Личный проект: модуляция (часть 2) - правки 3

--- js/main-pin.js
+++ js/main-pin.js
@@ -7,36 +7,27 @@
 (function () {
   var pinMain = window.utils.pinMain;
   var MAP_WIDTH = document.querySelector('.map__pins').offsetWidth;
+  var PIN_WIDTH = 62;
   var PIN_HEIGHT = 62;
   var PIN_HEIGHT_OFFSET = 22;
+  var MIN_WIDTH_MAP = -PIN_WIDTH / 2;
+  var MAX_WIDTH_MAP = MAP_WIDTH - PIN_WIDTH / 2;
   var MAP_HEIGHT_MIN = 130 - PIN_HEIGHT - PIN_HEIGHT_OFFSET;
   var MAP_HEIGHT_MAX = 630 - PIN_HEIGHT - PIN_HEIGHT_OFFSET;
-  var PIN_WIDTH = 62;
 
   pinMain.addEventListener('mousedown', function (evt) {
     evt.preventDefault();
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
+    var dragOffset = {
+      x: evt.clientX - pinMain.offsetLeft,
+      y: evt.clientY - pinMain.offsetTop
     };
-
 
     // Функция движения метки
     var onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
 
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      var newY = pinMain.offsetTop - shift.y;
-      var newX = pinMain.offsetLeft - shift.x;
+      var newY = moveEvt.clientY - dragOffset.y;
+      var newX = moveEvt.clientX - dragOffset.x;
 
       if (newY <= MAP_HEIGHT_MAX && newY >= MAP_HEIGHT_MIN) {
         pinMain.style.top = newY + 'px';
@@ -44,8 +35,20 @@
       if (newX <= MAP_WIDTH - PIN_WIDTH / 2 && newX >= -PIN_WIDTH / 2) {
         pinMain.style.left = newX + 'px';
       }
+      if (newX < MIN_WIDTH_MAP) {
+        newX = MIN_WIDTH_MAP;
+      }
+      if (newX > MAX_WIDTH_MAP) {
+        newX = MAX_WIDTH_MAP;
+      }
+      if (newY < MAP_HEIGHT_MIN) {
+        newY = MAP_HEIGHT_MIN;
+      }
+      if (newY > MAP_HEIGHT_MAX) {
+        newY = MAP_HEIGHT_MAX;
+      }
 
-      window.utils.inputAddress.value = pinMain.offsetLeft - shift.x + PIN_WIDTH / 2 + ', ' + (pinMain.offsetTop - shift.y + PIN_HEIGHT + PIN_HEIGHT_OFFSET);
+      window.utils.inputAddress.value = (newX + PIN_WIDTH / 2) + ', ' + (newY + PIN_HEIGHT + PIN_HEIGHT_OFFSET);
     };
 
     // Функция прекращения движения метки
